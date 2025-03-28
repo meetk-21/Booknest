b77e947f95e863e5716f338499380288df98961a
meet <meetm9087@gmail.com>
2025-03-28 09:49:34 +0000

    Fixed some mistakes

--- login.php
+++ login.php
@@ -19,8 +19,27 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 }
 ?>
 
-<form method="POST">
-    Username: <input type="text" name="username" required><br>
-    Password: <input type="password" name="password" required><br>
-    <input type="submit" value="Login">
-</form>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+
+<body>
+    <form method="POST">
+        <section>
+            <label for="username"> Username: </label>
+            <input type="text" name="username" id="username">
+        </section>
+        <section>
+            <label for="password"> Password: </label>
+            <input type="password" name="password" id="password">
+        </section>
+        <section>
+            <button> Submit </button>
+        </section>
+    </form>
+</body>
