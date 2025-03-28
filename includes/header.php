4576cd343c577f5242fb79a9a8b117dac71e4f7a
meet <meetm9087@gmail.com>
2025-03-28 11:37:07 +0000

    Added Header

---
+++ includes/header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+
+<body>
+    <header>
+        <nav>
+            <a href="index.php">Home</a>
+            <a href="browse.php">Browse</a>
+            <a href="login.php">Login</a>
+            <a href="register.php">Register</a>
+            <a href="review.php">Review</a>
+
+        </nav>
+    </header>
